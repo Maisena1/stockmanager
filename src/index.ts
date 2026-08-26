b122a3f9b0967f83de3165cc84f8c111ead1c610
Jeremias Mai <101605523+Maisena1@users.noreply.github.com>
2026-08-26 13:04:59 -0300

    Revert "chore Add auth dependencies and update config"

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,7 @@
 import "dotenv/config";
-import os from "os";
 import app from "./app";
 
 const PORT = process.env.PORT || 2060;
-
-function getNetworkIP(): string | undefined {
-  const nets = os.networkInterfaces();
-  for (const name of Object.keys(nets)) {
-    for (const net of nets[name] || []) {
-      if (net.family === "IPv4" && !net.internal) return net.address;
-    }
-  }
-  return undefined;
-}
-
-app.listen(PORT, "0.0.0.0", () => {
-  const ip = getNetworkIP();
-  console.log(`API running on http://localhost:${PORT}`);
-  if (ip) console.log(`Accessible on the network at http://${ip}:${PORT}`);
+app.listen(PORT, () => {
+    console.log(`API running on http://localhost:${PORT}`);
 });
